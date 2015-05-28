4d9c3de1efe61b9b6cd311f06b4fad0a1df9276f
arati <arati.nankar@planertria.com>
2015-05-28 16:02:04 +0530

    add the new files

--- models/predictivemodeling.js
+++ models/predictivemodeling.js
@@ -8,7 +8,16 @@ var Predictivemodeling = new keystone.List('Predictivemodeling', {
 Predictivemodeling.add({
 	image:{type:Types.CloudinaryImage},
 	text:{type:String},
-	subText:{ type: Types.Textarea, initial: true },
+	subText1:{ type: String },
+	subText2:{ type: String },
+	subText3:{ type: String },
+	subText4:{ type: String },
+	subText5:{ type: String },
+	subText6:{ type: String },
+	subText7:{ type: String },
+	subText8:{ type: String },
+	subText9:{ type: String },
+	subText10:{ type: String },
 	shortString:{ type: Types.Textarea, initial: true },
 	heading:{type:String},
 	
